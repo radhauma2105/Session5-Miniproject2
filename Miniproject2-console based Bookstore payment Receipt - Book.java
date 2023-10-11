7f4e86f1dadaac052e8eb3a495c9079e28e3aede
radhauma2105 <142169459+radhauma2105@users.noreply.github.com>
2023-10-11 17:46:35 +0530

    Create Miniproject2-console based Bookstore payment Receipt - Book.java

---
+++ Miniproject2-console based Bookstore payment Receipt - Book.java
@@ -0,0 +1,46 @@
+class Book {
+    private String title;
+    private String author;
+    private double price;
+    private int stock;
+
+    public Book(String title, String author, double price, int stock) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    // Getters and setters
+}
